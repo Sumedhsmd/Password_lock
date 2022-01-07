fc5c161c9ea4b5f8689a6c1b6b9d95ef3d86cb96
Sumedh-smd <90448315+Sumedh-smd@users.noreply.github.com>
2022-01-07 18:42:30 +0530

    intial commit

---
+++ Pass_lock.py
@@ -0,0 +1,18 @@
+password = 1729
+
+chances = 4
+
+print("Enter Pin : ")
+while chances >= 0:
+    pin = int(input(" > "))
+    if len(str(pin)) == 4:
+        if pin == password:
+            print("Correct Pin ....  ")
+            print("thank you")
+            break
+        else:
+            print("Incorrect Pin ....")
+            print(f"{chances} Chances Remaining ..")
+            chances -= 1
+    else:
+        print("Minimum 4 characters")
